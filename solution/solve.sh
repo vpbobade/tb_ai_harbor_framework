e60870a97e3d40204cba71148d072da092f20eed
root <root@AIPL7992LT06874.altimetrik.com>
2026-03-06 18:26:37 +0530

    fixed

--- solution/solve.sh
+++ solution/solve.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-bash fix_requirements.sh
-bash fix_env.sh
-bash fix_startup.sh
+bash /solution/fix_requirements.sh
+bash /solution/fix_env.sh
+bash /solution/fix_startup.sh
--- tests/test.sh
+++ tests/test.sh
@@ -14,6 +14,7 @@ source $HOME/.local/bin/env
 uvx \
   --with pytest==8.4.1 \
   --with pytest-json-ctrf==0.3.5 \
+  --with requests \
   pytest --ctrf /logs/verifier/ctrf.json /tests/test_outputs.py -rA
 
 if [ $? -eq 0 ]; then
